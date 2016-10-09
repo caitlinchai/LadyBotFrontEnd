fd05825a54ae563416cd7fd13094d2fe3dff11d3
caitlinchai <caitlinchai.2012@gmail.com>
2016-10-08 22:51:45 -0400

    create message bubbles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 var app = angular.module('LadyBot',[]);
 
 app.controller('MainCtrl', function($scope){
+  $scope.messages = [];
+  $scope.text = "";
+  $scope.enter = function(event){
+    if(event.which === 13 && $scope.text){
+      event.preventDefault();
+      $scope.messages.push({content: $scope.text, side: 'right'});
+      console.log($scope.messages);
+      $scope.text="";
+    }
+  };
+
   function updateTime(){
     var d = new Date();
     var hours = (d.getHours()-1)%12 + 1;
