53021561f41ab267406f2efa9be6438db2a7cc7d
caitlinchai <caitlinchai.2012@gmail.com>
2016-10-09 23:53:28 -0400

    make links clickable

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ app.controller('MainCtrl', function($scope, $timeout, $http){
   $scope.messages = [{content: "Hi! What's up? :)", side: "left"}];
   $scope.text = "";
   $scope.send = send;
+  updateTime();
+  setInterval(updateTime, 10000);
 
   $scope.enter = function(event){
     if(event.which === 13){
@@ -13,7 +15,6 @@ app.controller('MainCtrl', function($scope, $timeout, $http){
   };
 
   function send(){
-
     if(!$scope.text){
       return;
     }
@@ -25,7 +26,8 @@ app.controller('MainCtrl', function($scope, $timeout, $http){
       url:"http://localhost:3000/receive",
       params: {message: $scope.text}
     }).then(function success(response){
-      $scope.messages.push({content: response.data, side: "left"});
+      var message = getMessage(response.data);
+      $scope.messages.push(message);
       scrollToBottom();
     }, function myError(response){
       $scope.messages.push({content:"Brb.", side:"left"});
@@ -53,6 +55,16 @@ app.controller('MainCtrl', function($scope, $timeout, $http){
     var ampm = (d.getHours() > 12) ? "PM" : "AM"; 
     $scope.time = hours + ":" + minutes + " " + ampm;
   }
-  updateTime();
-  setInterval(updateTime, 10000);
+
+  function getMessage(str){
+    var link_index = str.indexOf('http');
+    if(link_index > -1){
+      var link = str.substring(link_index, str.length);
+      var content = str.substring(0, link_index);
+      return {content: content, side: "left", link: link};
+    }else{
+      return {content: str, side:"left"};
+    }
+  }
+
 });
